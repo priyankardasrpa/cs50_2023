18ca1a398a3f8985f1e29b0ce0dc9daeab944087
priyankardasrpa <111060663+priyankardasrpa@users.noreply.github.com>
2023-07-18 16:00:24 +0530

    Pass all testcases

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,35 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    // TODO: Prompt for height
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    // TODO: Render the pyramid
+    for (int row = 1; row <= height; row++)
+    {
+        int hashes = row;
+        int spaces = height - hashes;
+
+        // TODO: Print the spaces first
+        while (spaces > 0)
+        {
+            printf(" ");
+            spaces = spaces - 1;
+        }
+
+        // TODO: Print the hashes after spaces
+        while (hashes > 0)
+        {
+            printf("#");
+            hashes = hashes - 1;
+        }
+        printf("\n");
+    }
 }
